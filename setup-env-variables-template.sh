1d0d5edc351601ec52790593aa4d0814f8c4fa77
Eric Talla <eric.talla@keynetiks.com>
2023-10-26 19:39:05 +0200

    modified to maven compiler release 8

--- setup-env-variables-template.sh
+++ setup-env-variables-template.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Azure Environment
-export SUBSCRIPTION=your-subscription-id # customize this
-export RESOURCE_GROUP=migrate-java-ee-app-to-azure # you may want to customize by supplying a Resource Group name
-export WEBAPP=your-web-app-name # customize this - say, seattle-petstore
-export REGION=westus
+export SUBSCRIPTION= 759cb5fd-7dea-4d05-97ae-725f450c93d8 # customize this
+export RESOURCE_GROUP= NetworkWatcherRG # you may want to customize by supplying a Resource Group name
+export WEBAPP= etmwebapp # customize this - say, seattle-petstore
+export REGION=North Europe
 
-export DATABASE_SERVER=your-database-server-name # customize this
-export DATABASE_ADMIN=selvasingh # customize this
-export DATABASE_ADMIN_PASSWORD=SuperS3cr3t # customize this
+export DATABASE_SERVER=javaeeapp # customize this
+export DATABASE_ADMIN=Libosso # customize this
+export DATABASE_ADMIN_PASSWORD=Tall@dmin. # customize this
 
 # ======== DERIVED Environment Variable Values ===========
 
@@ -23,26 +23,26 @@ export POSTGRES_CONNECTION_URL=jdbc:postgresql://${POSTGRES_SERVER_FULL_NAME}:54
 export POSTGRES_SERVER_ADMIN_FULL_NAME=${POSTGRES_SERVER_ADMIN_LOGIN_NAME}@${POSTGRES_SERVER_NAME}
 
 # Composed secrets for MySQL
-export MYSQL_SERVER_NAME=mysql-${DATABASE_SERVER}
-export MYSQL_SERVER_ADMIN_LOGIN_NAME=${DATABASE_ADMIN}
-export MYSQL_SERVER_ADMIN_PASSWORD=${DATABASE_ADMIN_PASSWORD}
-export MYSQL_DATABASE_NAME=petstore
+###export MYSQL_SERVER_NAME=mysql-${DATABASE_SERVER}
+###export MYSQL_SERVER_ADMIN_LOGIN_NAME=${DATABASE_ADMIN}
+###export MYSQL_SERVER_ADMIN_PASSWORD=${DATABASE_ADMIN_PASSWORD}
+###export MYSQL_DATABASE_NAME=petstore
 
-export MYSQL_SERVER_FULL_NAME=${MYSQL_SERVER_NAME}.mysql.database.azure.com
-export MYSQL_CONNECTION_URL=jdbc:mysql://${MYSQL_SERVER_FULL_NAME}:3306/${MYSQL_DATABASE_NAME}?ssl=true\&useLegacyDatetimeCode=false\&serverTimezone=GMT
-export MYSQL_SERVER_ADMIN_FULL_NAME=${MYSQL_SERVER_ADMIN_LOGIN_NAME}\@${MYSQL_SERVER_NAME}
+###export MYSQL_SERVER_FULL_NAME=${MYSQL_SERVER_NAME}.mysql.database.azure.com
+###export MYSQL_CONNECTION_URL=jdbc:mysql://${MYSQL_SERVER_FULL_NAME}:3306/${MYSQL_DATABASE_NAME}?ssl=true\&useLegacyDatetimeCode=false\&serverTimezone=GMT
+###export MYSQL_SERVER_ADMIN_FULL_NAME=${MYSQL_SERVER_ADMIN_LOGIN_NAME}\@${MYSQL_SERVER_NAME}
 
 # Composed secrets for SQLServer
-export SQL_SERVER_NAME=sql-${DATABASE_SERVER}
-export SQL_SERVER_ADMIN_LOGIN_NAME=${DATABASE_ADMIN}
-export SQL_SERVER_ADMIN_PASSWORD=${DATABASE_ADMIN_PASSWORD}
-export SQL_DATABASE_NAME=petstore
+###export SQL_SERVER_NAME=sql-${DATABASE_SERVER}
+###export SQL_SERVER_ADMIN_LOGIN_NAME=${DATABASE_ADMIN}
+###export SQL_SERVER_ADMIN_PASSWORD=${DATABASE_ADMIN_PASSWORD}
+###export SQL_DATABASE_NAME=petstore
 
-export SQL_SERVER_FULL_NAME=${SQL_SERVER_NAME}.database.windows.net
-export SQL_SERVER_ADMIN_FULL_NAME=${SQL_SERVER_ADMIN_LOGIN_NAME}@${SQL_SERVER_NAME}
-export SQL_CONNECTION_URL="jdbc:sqlserver://${SQL_SERVER_FULL_NAME}:1433;database=${SQL_DATABASE_NAME};user=${SQL_SERVER_ADMIN_FULL_NAME};password=${SQL_SERVER_ADMIN_PASSWORD};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;"
+###export SQL_SERVER_FULL_NAME=${SQL_SERVER_NAME}.database.windows.net
+###export SQL_SERVER_ADMIN_FULL_NAME=${SQL_SERVER_ADMIN_LOGIN_NAME}@${SQL_SERVER_NAME}
+###export SQL_CONNECTION_URL="jdbc:sqlserver://${SQL_SERVER_FULL_NAME}:1433;database=${SQL_DATABASE_NAME};user=${SQL_SERVER_ADMIN_FULL_NAME};password=${SQL_SERVER_ADMIN_PASSWORD};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;"
 
-# Composed secrets for Azure Monitor, Log Analtyics and Application Insights
+# Composed secrets for Azure Monitor, Log Analtyics and Application Insight
 export LOG_ANALYTICS=${WEBAPP}
 export LOG_ANALYTICS_RESOURCE_ID= # will be set by script
 export WEBAPP_RESOURCE_ID= # will be set by script
